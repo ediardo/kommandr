0924b01b8bc627edcc0b547de5e399ac290cb020
Eddie Ramirez <djedi.r@gmail.com>
2017-09-06 13:16:52 -0700

    Added new action creators

--- src/redux/actions/commandLineActions.js
+++ src/redux/actions/commandLineActions.js
@@ -1,54 +1,28 @@
 import * as types from '../constants/ActionTypes';
 
-const addProgram = program => ({
-  type: types.ADD_PROGRAM,
+const setCli = cli => ({
+  type: types.SET_CLI,
   payload: {
-    ...program
+    ...cli
   }
 });
 
-const addOption = option => ({
-  type: types.ADD_OPTION,
-  payload: {
-    ...option
-  }
-});
-const setOptionValue = value => ({
-  type: types.SET_OPTION_VALUE,
-  payload: {
-    ...value
-  }
-});
 const setTitle = title => ({
   type: types.SET_TITLE,
   payload: {
     ...title
   }
 });
-const removeOption = option => ({
-  type: types.REMOVE_OPTION,
+
+const setDescription = description => ({
+  type: types.SET_DESCRIPTION,
   payload: {
-    ...option
+    ...description
   }
-});
-const replaceProgram = (pos, oldProgram, newProgram) => ({
-  type: types.REPLACE_PROGRAM,
-  payload: {
-    ...pos,
-    ...oldProgram,
-    ...newProgram
-  }
-})
-const resetCli = () => ({
-  type: types.RESET_CLI
 });
 
 export default {
-  addProgram,
-  addOption,
-  setOptionValue,
+  setCli,
   setTitle,
-  removeOption,
-  replaceProgram,
-  resetCli
+  setDescription
 }
