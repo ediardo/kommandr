74bcc11a24970eea36a9a77db4241e5b7ea328d3
Eddie Ramirez <djedi.r@gmail.com>
2017-09-07 11:08:03 -0700

    Stateless components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,37 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Route } from 'react-router-dom';
 
-import { Container } from 'reactstrap';
-
 import CommandLine from '../components/CommandLine';
 import Contact from '../components/Contact';
-import Content from './Content';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Main from './Main';
 import Privacy from '../components/Privacy';
-import Sidebar from './Sidebar';
 import Terms from '../components/Terms';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <Header />
+const App = (props) => {
+  return (
+    <div className="app">
+      <Header />
         <Route path="/" exact component={Main} />
         <Route exact path="/new" component={CommandLine} />
         <Route path="/privacy" component={Privacy} />
         <Route path="/terms" component={Terms} />
         <Route path="/contact" component={Contact} />
-        <Footer />
-      </div>
-    )
-  }
-}
+      <Footer />
+    </div>
+  )
+};
 
 export default App;
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react';
+import React  from 'react';
 
-import { Container, Row, Col } from 'reactstrap';
+import { Container } from 'reactstrap';
 
 import Content from './Content';
 
-class Main extends Component {
-  constructor(props){
-    super(props)
-  }
-
-  render() {
-    return (
-      <Container fluid>
-        <Content className="main">
-          <h1>Welcome</h1>
-        </Content>
-      </Container>
-    )
-  }
+const Main = (props) => {
+  return (
+    <Container fluid>
+      <Content className="main">
+        <h1>Welcome</h1>
+      </Content>
+    </Container>
+  )
 }
 
 export default Main;
