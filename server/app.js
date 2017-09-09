4da638f363387d4cb63191361b0a2a8e3cc0759b
Eddie Ramirez <djedi.r@gmail.com>
2017-09-09 15:53:06 -0700

    Conf routes and express app

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var kommandrs = require('./routes/kommandrs');
 
 var app = express();
 
@@ -24,7 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+app.use('/kommandrs', kommandrs);
+/*
+app.use('/comments', comments);
+app.use('/groups', groups);
+app.use('/collections', collections);
+*/
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router
+  .get('/', (req, res) => {
+    models.User.findAll({
+      include: [],
+    }).then(function(users) {
+      res.json(users)
+    });
+  })
+  .post('/', (req, res) => {
+    const { username, email, password, github } = req.body;
+    models.User.create({
+      username,
+      email,
+      password,
+      github,
+    });
+  })
+  .get('/:userId', (req, res) => {
+    const { userId } = req.params;
+    models.User.findById(userId)
+      .then( user => {
+        res.json(user)
+    });
 });
 
 module.exports = router;
